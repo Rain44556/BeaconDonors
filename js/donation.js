03780ae4424102010a77f5c62d7d3c759c9c4e61
rain44556 <bristy44556@gmail.com>
2024-09-23 23:10:33 +0600

    alert added

--- js/donation.js
+++ js/donation.js
@@ -1,40 +1,33 @@
 
- function addClickListener(btnId, inputId, donationId){
-    document.getElementById(btnId).addEventListener('click', function(event){
+ function addClickListener(buttonId, inputId, donationId){
+    document.getElementById(buttonId).addEventListener('click', function(event){
         event.preventDefault();
     
-        const quotaInjuredDonation = getInputValueById(inputId);
+        const donationAmount = getInputValueById(inputId);
         const addDonation = getInnerTextValueById(donationId);
         const mainBalance = getInnerTextValueById('main-balance');
+
+        // alert
+        if(isNaN(donationAmount) || donationAmount < 0 || mainBalance < donationAmount){
+            alert("Failed to add amount");
+            return;
+        }
+     
     
-        const updateDonation = quotaInjuredDonation + addDonation;
+        const updateDonation = donationAmount + addDonation;
         document.getElementById(donationId).innerText = updateDonation;
     
-        const updateBalanceAmount = mainBalance - quotaInjuredDonation;
+        const updateBalanceAmount = mainBalance - donationAmount;
         document.getElementById('main-balance').innerText = updateBalanceAmount;
     
         document.getElementById(inputId).value = '';
-        document.getElementById(my_modal).style.display = block;
+        // modal
+        document.getElementById("my_modal").showModal();
     });
 }
+
 addClickListener('btn-quota-injured', 'quota-injured-donation-input', 'quota-injured-donation-addMoney');
 addClickListener('btn-noakhali', 'noakhali-donation-input', 'noakhali-donation-addMoney');
 addClickListener('btn-comilla', 'comilla-donation-input', 'comilla-donation-addMoney');
 addClickListener('btn-feni', 'feni-donation-input', 'feni-donation-addMoney');
-
-// document.getElementById('btn-quota-injured').addEventListener('click', function(event){
-//     event.preventDefault();
-
-//     const quotaInjuredDonation = getInputValueById('quota-injured-donation-input');
-//     const addDonation = getInnerTextValueById('quota-injured-donation-addMoney');
-//     const mainBalance = getInnerTextValueById('main-balance');
-
-//     const updateDonation = quotaInjuredDonation + addDonation;
-//     document.getElementById('quota-injured-donation-addMoney').innerText = updateDonation;
-
-//     const updateBalanceAmount = mainBalance - quotaInjuredDonation;
-//     document.getElementById('main-balance').innerText = updateBalanceAmount;
-
-//     document.getElementById('quota-injured-donation-input').value = '';
-// });
 
